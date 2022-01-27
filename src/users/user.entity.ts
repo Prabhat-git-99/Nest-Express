9cab367255020a5ceed06eea11503a705c82767c
Prabhat-git-99 <prabatwork3939@gmail.com>
2022-01-27 21:44:36 +0530

    WIP: DB Hooks Added

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from "typeorm";
 
 @Entity( )
 export class User {
@@ -12,5 +12,19 @@ export class User {
     @Column( )
     password: string;
 
+    @AfterInsert( )
+    logInsert( ) {
+        console.log( 'Inserted User with id ', this.id );
+    }
+
+    @AfterUpdate( )
+    logUpdate( ) {
+        console.log( 'Update User with id ', this.id );
+    }
+
+    @AfterRemove( )
+    logRemove( ) {
+        console.log( 'Removed User with id ', this.id );
+    }
 
 }

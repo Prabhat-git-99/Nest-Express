bcf6c95fabf931e2b84e22c9e59ebf4244717acb
Prabhat-git-99 <prabatwork3939@gmail.com>
2022-02-06 13:18:51 +0530

    WIP: Updated App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'process';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Report } from './reports/report.entity';
@@ -8,12 +10,29 @@ import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true
-  }),ReportsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot( { 
+      isGlobal: true,
+      envFilePath: `.env.${ process.env.NODE_ENV }`
+    } ),
+    TypeOrmModule.forRootAsync( { 
+      inject: [ ConfigService ],
+      useFactory: ( config: ConfigService ) => {
+        return {
+          type: 'sqlite',
+          database: config.get< string >( 'DB_NAME' ),
+          synchronize: true,
+          entities: [ User, Report ],
+        }
+      }
+    } ),
+  //   TypeOrmModule.forRoot({
+  //   type: 'sqlite',
+  //   database: 'db.sqlite',
+  //   entities: [User, Report],
+  //   synchronize: true
+  // }),
+  ReportsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
